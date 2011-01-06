25154aa68212e4e875acf4e2ca32d77772f6c058
Mathieu Poumeyrol <kali@fotonauts.com>
2011-01-06 11:52:49 +0100

    make lackr configurable through property file

--- src/main/java/com/fotonauts/lackr/TextSubstitutionEngine.java
+++ src/main/java/com/fotonauts/lackr/TextSubstitutionEngine.java
@@ -8,13 +8,13 @@ public abstract class TextSubstitutionEngine implements SubstitutionEngine {
 
 	protected boolean parseable(LackrRequest lackrRequest) {
 		String mimeType = lackrRequest.rootExchange.getResponseFields().getStringField("Content-Type");
-		return mimeType.startsWith("text/html")
+		return mimeType != null && (mimeType.startsWith("text/html")
 				|| mimeType.startsWith("application/xml")
 				|| mimeType.startsWith("application/json")
 				|| mimeType.startsWith("application/atom+xml")
 				|| mimeType.startsWith("text/javascript")
 				|| mimeType.startsWith("application/x-mmtml")
-				|| mimeType.startsWith("application/x-mmtml+xml");
+				|| mimeType.startsWith("application/x-mmtml+xml"));
 	}
 
 }
