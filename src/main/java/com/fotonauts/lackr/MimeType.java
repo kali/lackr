df5e162d503c0c5dcff7ae6951b5552e3900efb1
Mathieu Poumeyrol <kali@fotonauts.com>
2011-03-31 14:20:56 +0200

    forgot this file yersterday

---
+++ src/main/java/com/fotonauts/lackr/MimeType.java
@@ -0,0 +1,22 @@
+package com.fotonauts.lackr;
+
+public class MimeType {
+
+	public static String APPLICATION_ATOM_XML = "application/atom";
+	public static String APPLICATION_JAVASCRIPT = "application/javascript";
+	public static String APPLICATION_JSON = "application/json";
+	public static String APPLICATION_X_MMTML = "application/x-mmtml";
+	public static String APPLICATION_XML = "application/xml";
+	public static String TEXT_HTML = "text/html";
+	public static String TEXT_JAVASCRIPT = "text/javascript";
+
+	public static boolean isML(String mimeType) {
+		return mimeType.startsWith(TEXT_HTML) || mimeType.startsWith(APPLICATION_ATOM_XML)
+		        || mimeType.startsWith(APPLICATION_X_MMTML) || mimeType.startsWith(APPLICATION_XML);
+	}
+	
+	public static boolean isJS(String mimeType) {
+		return mimeType.startsWith(APPLICATION_JAVASCRIPT) || mimeType.startsWith(APPLICATION_JSON)
+        || mimeType.startsWith(TEXT_JAVASCRIPT);
+	}
+}
