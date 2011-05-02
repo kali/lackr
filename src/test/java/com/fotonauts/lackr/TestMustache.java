f2f2d437e5ad94dc6d84bc6f12e92e0abcfd1b76
Mathieu Poumeyrol <kali@fotonauts.com>
2011-05-02 20:49:40 +0200

    readd mustache+esi test

--- src/test/java/com/fotonauts/lackr/TestMustache.java
+++ src/test/java/com/fotonauts/lackr/TestMustache.java
@@ -22,4 +22,16 @@ public class TestMustache extends BaseTestSubstitution {
 		assertEquals("\n\nsome text from the template name:the name value:the value\n\n", result);
 	}
 
+	@Test
+	public void testMustacheWithEsi() throws Exception {
+		String result = expand(
+				"<!-- lackr:mustache:template name=\"template_name\" -->\n"
+				+ "some text from the template name:{{name}} value:{{value}} some:{{esi.some}}\n"
+				+ "<!-- /lackr:mustache:template -->\n"
+				+ "<!-- lackr:mustache:eval name=\"template_name\" -->\n"
+				+ "{ \"name\": \"the name\", \"value\": \"the value\", \"esi\":\"ssi:include:virtual:/esi.json\" }\n"
+				+ "<!-- /lackr:mustache:eval -->\n");
+		assertEquals("\n\nsome text from the template name:the name value:the value some:json crap\n\n", result);
+	}
+
 }
