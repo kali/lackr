ddf83981e7d6f40969aac7219ea28e4c926f8a9a
Mathieu Poumeyrol <kali@fotonauts.com>
2012-03-21 14:24:27 +0100

    warning and cosmetic

--- src/main/java/com/fotonauts/lackr/esi/AbstractMLESIRule.java
+++ src/main/java/com/fotonauts/lackr/esi/AbstractMLESIRule.java
@@ -1,7 +1,6 @@
 package com.fotonauts.lackr.esi;
 
 import com.fotonauts.lackr.BackendRequest;
-import com.fotonauts.lackr.LackrBackendExchange;
 import com.fotonauts.lackr.LackrPresentableError;
 import com.fotonauts.lackr.MimeType;
 import com.fotonauts.lackr.esi.filters.AmpersandEscapeChunk;
@@ -9,33 +8,31 @@ import com.fotonauts.lackr.interpolr.Chunk;
 
 public abstract class AbstractMLESIRule extends ESIIncludeRule {
 
-    public AbstractMLESIRule(String markup) {
-        super(markup);
-    }
+	public AbstractMLESIRule(String markup) {
+		super(markup);
+	}
 
-    @Override
-    public String getSyntaxIdentifier() {
-        return "ML";
-    }
+	@Override
+	public String getSyntaxIdentifier() {
+		return "ML";
+	}
 
-    @Override
-    public Chunk filterDocumentAsChunk(BackendRequest request) {
-        String mimeType = getMimeType(request.getExchange());
-        // JS is detected by check()
-        if (MimeType.isML(mimeType))
-            return request.getParsedDocument();
-        else
-            // so this is most likely plain text
-            return new AmpersandEscapeChunk(request.getParsedDocument());
-    }
+	@Override
+	public Chunk filterDocumentAsChunk(BackendRequest request) {
+		String mimeType = getMimeType(request.getExchange());
+		// JS is detected by check()
+		if (MimeType.isML(mimeType))
+			return request.getParsedDocument();
+		else
+			// so this is most likely plain text
+			return new AmpersandEscapeChunk(request.getParsedDocument());
+	}
 
-    @Override
-    public void check(BackendRequest request) {
-        String mimeType = getMimeType(request.getExchange());
-        if (MimeType.isJS(mimeType))
-            request
-                    .getFrontendRequest()
-                    .addBackendExceptions(
-                            new LackrPresentableError("unsupported ESI type (js* in *ML context)", request.getExchange()));
-    }
+	@Override
+	public void check(BackendRequest request) {
+		String mimeType = getMimeType(request.getExchange());
+		if (MimeType.isJS(mimeType))
+			request.getFrontendRequest().addBackendExceptions(
+			        new LackrPresentableError("unsupported ESI type (js* in *ML context)", request.getExchange()));
+	}
 }
