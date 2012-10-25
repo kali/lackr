b312d2b4f3f7575aeeda53813a1b48da93d3bc84
Mathieu Poumeyrol <kali@fotonauts.com>
2012-10-25 13:42:32 +0200

    more servlet api compliant

--- src/main/java/com/fotonauts/lackr/femtor/FemtorRequest.java
+++ src/main/java/com/fotonauts/lackr/femtor/FemtorRequest.java
@@ -20,124 +20,126 @@ import com.fotonauts.lackr.BackendRequest;
 
 public class FemtorRequest extends HttpServletRequestWrapper {
 
-	BackendRequest request;
-	private HttpFields headers = new HttpFields();
+    BackendRequest request;
+    private HttpFields headers = new HttpFields();
     private MultiMap<String> params;
     private BufferedReader reader;
-	private ServletInputStream inputStream;
+    private ServletInputStream inputStream;
 
-	public FemtorRequest(HttpServletRequest httpServletRequest, BackendRequest request) {
-		super(httpServletRequest);
-		this.request = request;
-	}
-	
-	@Override
-	public BufferedReader getReader() throws IOException {
-		if(inputStream != null)
-			throw new java.lang.IllegalStateException("Already streaming as an input stream");
-		if(reader == null)
-			reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(request.getBody()), "UTF-8"));
-		return reader;
-	}
-	
-	@Override
-	public ServletInputStream getInputStream() throws IOException {
-		if(reader != null)
-			throw new java.lang.IllegalStateException("Already streaming as a reader");		
-		if(inputStream == null)
-			inputStream = new ServletInputStream() {
-				
-				ByteArrayInputStream is = new ByteArrayInputStream(request.getBody());
-			
-				@Override
-				public int read() throws IOException {
-					return is.read();
-				}
-			};
-		return inputStream;
-	}
-	
-	@Override
-	public String getMethod() {
-		return request.getMethod();
-	}
+    public FemtorRequest(HttpServletRequest httpServletRequest, BackendRequest request) {
+        super(httpServletRequest);
+        this.request = request;
+    }
 
-	@Override
-	public String getPathInfo() {
-		return request.getPath();
-	}
-
-	@Override
-	public String getQueryString() {
-		return request.getParams();
-	}
-
-	@Override
-	public String getRequestURI() {
-		return request.getPath();
-	}
-
-	public void addHeader(String name, String value) {
-	    headers.add(name, value);
-	}
-
-	protected MultiMap<String> getParams() {
-	    if(params == null) {
-	        params = new MultiMap<String>();
-	        UrlEncoded.decodeTo(getQueryString(), params, "UTF-8");
-	    }
-	    return params;
-	}
-	
-	@SuppressWarnings("rawtypes")
     @Override
-	public Enumeration getParameterNames() {
-        if(request.getFrontendRequest().getRootRequest() == request)
+    public BufferedReader getReader() throws IOException {
+        if (inputStream != null)
+            throw new java.lang.IllegalStateException("Already streaming as an input stream");
+        if (reader == null)
+            reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(request.getBody()), "UTF-8"));
+        return reader;
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (reader != null)
+            throw new java.lang.IllegalStateException("Already streaming as a reader");
+        if (inputStream == null)
+            inputStream = new ServletInputStream() {
+
+                ByteArrayInputStream is = new ByteArrayInputStream(request.getBody());
+
+                @Override
+                public int read() throws IOException {
+                    return is.read();
+                }
+            };
+        return inputStream;
+    }
+
+    @Override
+    public String getMethod() {
+        return request.getMethod();
+    }
+
+    @Override
+    public String getPathInfo() {
+        return request.getPath();
+    }
+
+    @Override
+    public String getQueryString() {
+        return request.getParams();
+    }
+
+    @Override
+    public String getRequestURI() {
+        return request.getPath();
+    }
+
+    public void addHeader(String name, String value) {
+        headers.add(name, value);
+    }
+
+    protected MultiMap<String> getParams() {
+        if (params == null) {
+            params = new MultiMap<String>();
+            UrlEncoded.decodeTo(getQueryString(), params, "UTF-8");
+        }
+        return params;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getParameterNames() {
+        if (request.getFrontendRequest().getRootRequest() == request)
             return super.getParameterNames();
         else
             return Collections.enumeration(getParams().keySet());
-	}
-	
-	@Override
-	public String getParameter(String name) {
-	    if(request.getFrontendRequest().getRootRequest() == request)
-	        return super.getParameter(name);
-	    else
-	        return getParams().getString(name);
-	}
-	
-	@Override
-	public String[] getParameterValues(String name) {
-        if(request.getFrontendRequest().getRootRequest() == request)
-            return super.getParameterValues(name);
+    }
+
+    @Override
+    public String getParameter(String name) {
+        if (request.getFrontendRequest().getRootRequest() == request)
+            return super.getParameter(name);
         else
-	    return getParams().getValues(name).toArray(new String[0]);
-	}
-	
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Map getParameterMap() {
-        if(request.getFrontendRequest().getRootRequest() == request)
+            return getParams().getString(name);
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        if (request.getFrontendRequest().getRootRequest() == request)
+            return super.getParameterValues(name);
+        else if (getParams() != null && getParams().getValues(name) != null)
+            return getParams().getValues(name).toArray(new String[0]);
+        else
+            return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Map getParameterMap() {
+        if (request.getFrontendRequest().getRootRequest() == request)
             return super.getParameterMap();
         else
-        return getParams().toStringArrayMap();
-	    
-	}
-	
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Enumeration getHeaderNames() {
-	    return headers.getFieldNames();
-	}
+            return getParams().toStringArrayMap();
 
-	@Override
-	public String getHeader(String name) {
-	    return headers.getStringField(name);
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Enumeration getHeaders(String name) {
-	    return headers.getValues(name);
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getHeaderNames() {
+        return headers.getFieldNames();
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return headers.getStringField(name);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Enumeration getHeaders(String name) {
+        return headers.getValues(name);
+    }
 }
