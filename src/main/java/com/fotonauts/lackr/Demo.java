834e806d27f17dd1917e550daa4d5a2ee42b89b2
Mathieu Poumeyrol <kali@fotonauts.com>
2014-03-10 14:06:21 +0100

    restructure doc

--- src/main/java/com/fotonauts/lackr/Demo.java
+++ src/main/java/com/fotonauts/lackr/Demo.java
@@ -1,5 +1,8 @@
 package com.fotonauts.lackr;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 
@@ -8,19 +11,43 @@ import com.fotonauts.lackr.testutils.Factory;
 
 public class Demo {
 
+    public static int findFreeTCPPort() {
+        for (int port = 8000; port < 8100; port++) {
+            ServerSocket ss = null;
+            try {
+                ss = new ServerSocket(port);
+                ss.setReuseAddress(true);
+                return port;
+            } catch (IOException e) {
+            } finally {
+                if (ss != null) {
+                    try {
+                        ss.close();
+                    } catch (IOException e) {
+                        /* should not be thrown */
+                    }
+                }
+            }
+        }
+        throw new RuntimeException("No available port found");
+
+    }
+
     public static void main(String[] args) throws Exception {
         String backendUrl = "http://localhost/~" + System.getProperty("user.name") + "/";
-        if (args.length > 1 && args[1] != null && !"".equals(args[1]))
-            backendUrl = args[1];
-        
+        if (args.length > 0 && args[0] != null && !"".equals(args[0]))
+            backendUrl = args[0];
+
         Backend backend = Factory.buildFullClientBackend(backendUrl, null);
         Interpolr interpolr = Factory.buildInterpolr("handlebars esi");
-        
-        Server server = Factory.buildInterpolrProxyServer(interpolr, backend);
+
+        int port = findFreeTCPPort();
+        Server server = Factory.buildInterpolrProxyServer(interpolr, backend, port);
 
         server.start();
-        System.out.println("Backend is " + backendUrl);
-        System.out.println("Proxy Server listenning on http://localhost:" + ((ServerConnector) (server.getConnectors()[0])).getLocalPort() + "/");
+        System.out.println("Backend is: " + backendUrl);
+        System.out.println("Proxy Server listenning on http://localhost:"
+                + ((ServerConnector) (server.getConnectors()[0])).getLocalPort() + "/");
         server.join();
     }
 
